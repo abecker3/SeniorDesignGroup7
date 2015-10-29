6796722b7ca17ffe8ba3e3b4f3846b6b8bd5e40c
Taras Kravchuk <MostWanted@TKMP.local>
2015-10-29 13:34:16 -0700

    adding project

---
+++ ProvidenceWayfinding/ProvidenceWayfinding/MenuViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MenuViewController.swift
+//  MapTest
+//
+//  Created by Taras Kravchuk on 10/27/15.
+//  Copyright © 2015 Taras Kravchuk. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    let transitionManager = TransitionManager()
+
+    @IBOutlet weak var Logo: UIImageView!
+    @IBOutlet weak var DirectHospital: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        // this gets a reference to the screen that we're about to transition to
+        let toViewController = segue.destinationViewController as UIViewController
+        
+        // instead of using the default transition animation, we'll ask
+        // the segue to use our custom TransitionManager object to manage the transition animation
+        toViewController.transitioningDelegate = self.transitionManager
+        
+    }
+    
+    @IBAction func unwindToViewController (sender: UIStoryboardSegue){
+        
+    }
+    
+    // MARK: - Navigation
+/*
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }*/
+
+
+}
