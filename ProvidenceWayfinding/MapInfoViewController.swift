644f9bfc0ba9fec613cc94e5f2b853ba81c933ad
Kravchuk <kravchuk@PC106-macH.local>
2015-11-11 16:46:45 -0800

    update branch

---
+++ ProvidenceWayfinding/MapInfoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MapInfoViewController.swift
+//  ProvidenceWayfinding
+//
+//  Created by Kravchuk, Taras on 11/10/15.
+//  Copyright © 2015 GU. All rights reserved.
+//
+
+import UIKit
+
+class MapInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.translucent = false
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
