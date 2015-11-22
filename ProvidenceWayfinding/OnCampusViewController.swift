7451b3b9ed2f0f272088e5db95c26208ea588c94
Becker, Andrew D <abecker3@pc106-maco.local>
2015-11-22 15:02:42 -0800

    So far working on campus

--- ProvidenceWayfinding/OnCampusViewController.swift
+++ ProvidenceWayfinding/OnCampusViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class OnCampusViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
+class OnCampusViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UIScrollViewDelegate{
+    @IBOutlet weak var scrollMap: UIScrollView!
+    @IBOutlet weak var floorMap: UIImageView!
     @IBOutlet weak var picker: UIPickerView!
     
     var pickerData: [[String]] = [[String]]()
@@ -18,19 +19,28 @@ class OnCampusViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        floorMap.image = UIImage(named:"Campus.jpg")
+        floorMap.contentMode = UIViewContentMode.ScaleAspectFit
+        
+        self.scrollMap.maximumZoomScale = 5.0
+        self.scrollMap.clipsToBounds = true
+        
         // Connect data:
         self.picker.delegate = self
         self.picker.dataSource = self
         
         // Input data into the Array:
         pickerData = [["None","Children Emergency", "Bone/Joint", "Cardiac", "Gynecology", "Fetal-Medicine", "Item 6"],
-                    ["None","Campus","Childrens", "Heart Ins", "Main", "Womens"],
-                    ["None","1","2","3","4"]]
+                    ["None","Floor 1","Floor 2","Floor 3","Floor 4"]]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
     // The number of columns of data
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 3
+        return 2
     }
     
     // The number of rows of data
@@ -48,4 +58,9 @@ class OnCampusViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         // This method is triggered whenever the user makes a change to the picker selection.
         // The parameter named row and component represents what was selected.
     }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return self.floorMap
+    }
+    
 }
