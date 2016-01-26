96c10ac1215e7714ae28c6497b8fb13f32212999
Ruth Manthey <rmanthey@zagmail.gonzaga.edu>
2016-01-26 12:32:07 -0800

    Updated directory formatting

---
+++ ProvidenceWayfinding/ProvidenceWayfinding/DirectorySpecificViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DirectorySpecificViewController.swift
+//  ProvidenceWayfinding
+//
+//  Created by Ruth Manthey on 1/26/16.
+//  Copyright © 2016 GU. All rights reserved.
+//
+
+import UIKit
+
+class DirectorySpecificViewController: UIViewController {
+
+}
