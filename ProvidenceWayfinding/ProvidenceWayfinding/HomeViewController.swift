7aa53ef3209141b4de01352f0d0bfec7fa681632
Derek Becker <derekbecker@derek-beckers-macbook-pro.local>
2016-02-05 10:44:10 -0800

    Working swipe views

--- ProvidenceWayfinding/ProvidenceWayfinding/HomeViewController.swift
+++ ProvidenceWayfinding/ProvidenceWayfinding/HomeViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    var screenEdgeRecognizer: UIScreenEdgePanGestureRecognizer!
+    var flag = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        screenEdgeRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: "switchScreenGesture:")
+        screenEdgeRecognizer.edges = .Right
+        view.addGestureRecognizer(screenEdgeRecognizer)
     }
     
+    func switchScreenGesture(sender: UIScreenEdgePanGestureRecognizer) {
+        flag = flag + 1
+        if (flag % 2 == 1){
+            tabBarController?.selectedIndex = 1
+        }
+    }
 
 }
