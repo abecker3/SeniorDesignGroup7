31310f7a9531838d52198e847a2702dac09d01fa
Shaun McBurney <smcburney@zagmail.gonzaga.edu>
2016-02-06 12:52:39 -0800

    made a search all locations table cell in the survey path through our app

--- ProvidenceWayfinding/ProvidenceWayfinding/Location.swift
+++ ProvidenceWayfinding/ProvidenceWayfinding/Location.swift
@@ -56,4 +56,6 @@ let locations = [
     Location(name: "Waiting Room (Pediatric Surgery/Family)",                           category: "Main Tower",                     floor: "L1"),
     Location(name: "Waiting Room (Skybridge)",                                          category: "Main Tower",                     floor: "L1"),
     Location(name: "Waiting Room (Surgery)",                                            category: "Surgery Center",                 floor: "Main"),
+    Location(name: "Search All Locations",                                     category: "Search All Locations",
+        floor: "kdjlsa")
 ]
