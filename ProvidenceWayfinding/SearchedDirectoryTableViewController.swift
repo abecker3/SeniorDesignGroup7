58879af36db9c72dd9533772d8246820ab6fae74
Ruth Manthey <rmanthey@zagmail.gonzaga.edu>
2016-02-14 17:31:02 -0800

    Working on second directory screen

---
+++ ProvidenceWayfinding/SearchedDirectoryTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchedDirectoryTableViewController.swift
+//  ProvidenceWayfinding
+//
+//  Created by Ruth Manthey on 2/14/16.
+//  Copyright © 2016 GU. All rights reserved.
+//
+
+import UIKit
+
+class SearchedDirectoryTableViewController: UITableViewController {
+    
+}
