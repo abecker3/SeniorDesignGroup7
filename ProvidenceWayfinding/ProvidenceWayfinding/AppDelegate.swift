bdaab18d9aa74900053b23203590019a8a3bf76f
Derek Becker <derekbecker@derek-beckers-macbook-pro.local>
2016-03-19 15:18:11 -0700

    Working

--- ProvidenceWayfinding/ProvidenceWayfinding/AppDelegate.swift
+++ ProvidenceWayfinding/ProvidenceWayfinding/AppDelegate.swift
@@ -21,6 +21,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         pageControl.pageIndicatorTintColor = UIColor.lightGrayColor()
         pageControl.currentPageIndicatorTintColor = UIColor.blackColor()
         pageControl.backgroundColor = UIColor.whiteColor()
+    
+        //UITabBar.appearance().barTintColor = UIColor(red: 244/255, green: 133/255, blue: 24/255, alpha: 1.0)
+        UINavigationBar.appearance().barTintColor = UIColor(red: 6/255, green: 56/255, blue: 122/255, alpha: 1.0)
+        UITabBar.appearance().barTintColor = UIColor(red: 6/255, green: 56/255, blue: 122/255, alpha: 1.0)
+        
+        UINavigationBar.appearance().tintColor = UIColor(red: 244/255, green: 133/255, blue: 24/255, alpha: 1.0)
+        UITabBar.appearance().tintColor = UIColor(red: 244/255, green: 133/255, blue: 24/255, alpha: 1.0)
+        
+        UISegmentedControl.appearance().tintColor = UIColor(red: 6/255, green: 56/255, blue: 122/255, alpha: 1.0)
         
         return true		
     }
