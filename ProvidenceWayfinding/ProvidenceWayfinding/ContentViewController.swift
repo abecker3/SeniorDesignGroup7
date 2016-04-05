3bb75ec6019a7ea1d4bc8bc0e0f4c969f7c16203
Derek Becker <derekbecker@derek-beckers-macbook-pro.local>
2016-04-05 12:57:09 -0700

    Fixed tutorial and parking path

--- ProvidenceWayfinding/ProvidenceWayfinding/ContentViewController.swift
+++ ProvidenceWayfinding/ProvidenceWayfinding/ContentViewController.swift
@@ -35,13 +35,13 @@ class ContentViewController: UIViewController {
             textField.text = "From the Home tab you can: \n\n1) View the tutorial to learn the functionality of the app \n\n2) Read the mission of Providence Sacred Heart"
         }
         else if (titleText == "Directions"){
-            textField.text = "From the Directions tab you can: \n\n1) View general tips about navigating the Hospital by clicking the 'Completely Lost?' button \n\n2) View all available floor maps by clicking the 'Floor Maps' button \n\n3) To get directions click the 'Route Now' button \n\tA) If your current location was 'Off Campus' the app will: \n\t\ta) Guide you to the best parking garage for your destination \n\t\t\ti) To view the written directions you can click the button on the right that resembles four lines \n\t\t\tii) There is an option at the bottom to use Apple Maps so that you can have voice navigation to the parking garage entrance \n\t\tb) The app will then prompt you to save your parking location \n\tB) The app will then begin navigating you through the hospital to your destination"
+            textField.text = "From the Directions tab you can: \n\n1) View general tips about navigating the Hospital by clicking the 'Completely Lost?' button \n\n2) View all available floor maps by clicking the 'Floor Maps' button \n\n3) To get directions click the 'Route Now' button"
         }
         else if (titleText == "Parking"){
-            textField.text = "From the Parking tab you can: \n\n1) Save your parking location \n\n2) View your most recent saved parking location \n\n3) Click on the History button to view the last 10 saved parking locations"
+            textField.text = "From the Parking tab you can: \n\n1) Save your parking location \n\n2) View your most recent saved parking location \n\n3) Click on the History button to view the last 10 saved parking locations \n\n4) Click Route to Car to be directed towards your car"
         }
         else if (titleText == "More"){
-            textField.text = "From the More tab you can: \n\n1) View the directory which allows you to search and view all locations that are supported in the app to learn: \n\tA) The phone extension \n\tB) The hours \n\tC) What building and floor the location is on \n\n2) View a map of the entire campus \n\n3) Read the terms of service"
+            textField.text = "From the More tab you can: \n\n1) View information about all locations supported by this App \n\n2) View a map of the entire campus \n\n3) Read the terms of service"
         }
         
         textField.font = UIFont(name: "Helvetica", size: 16)
