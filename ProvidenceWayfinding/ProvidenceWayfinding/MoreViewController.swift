a67fa21ab45b4ef3cd7553793810dfffb34c8f48
Shaun McBurney <smcburney@zagmail.gonzaga.edu>
2016-04-17 11:44:47 -0700

    Working scroll view with sliding pictures

--- ProvidenceWayfinding/ProvidenceWayfinding/MoreViewController.swift
+++ ProvidenceWayfinding/ProvidenceWayfinding/MoreViewController.swift
@@ -8,19 +8,23 @@
 
 import UIKit
 
-class MoreViewController: UIViewController {
+class MoreViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var scrollView: UIScrollView!
     var screenEdgeRecognizer: UIScreenEdgePanGestureRecognizer!
+    var imageViewHeight: CGFloat = 0
     var flag = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        scrollView.contentOffset = CGPointZero
+        
         screenEdgeRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: "switchScreenGesture:")
         screenEdgeRecognizer.edges = .Left
         view.addGestureRecognizer(screenEdgeRecognizer)
         
+        scrollView.frame = CGRectMake(0, 0, view.frame.width, view.frame.height * 0.6)
         let scrollViewWidth = scrollView.frame.width
         let scrollViewHeight = scrollView.frame.height
         //print("scrollViewWidth = " + String(scrollViewWidth))
@@ -28,11 +32,15 @@ class MoreViewController: UIViewController {
         
         //3
         let imgThree = UIImageView(frame: CGRectMake(scrollViewWidth*0, 0, scrollViewWidth, scrollViewHeight))
-        imgThree.image = UIImage(named: "Map.png")
+        imgThree.contentMode = .ScaleAspectFit
+        imageViewHeight = imgThree.frame.height
+        imgThree.image = UIImage(named: "providence_logo1.png")
         let imgFour = UIImageView(frame: CGRectMake(scrollViewWidth*1, 0,scrollViewWidth, scrollViewHeight))
-        imgFour.image = UIImage(named: "Car.png")
-        print("imgFour.image.size = " + String(imgFour.image!.size))
-        /*
+        imgThree.contentMode = .ScaleAspectFit
+
+        imgFour.image = UIImage(named: "childrensHospitalAtNight.jpg")
+        //print("imgFour.image.size = " + String(imgFour.image!.size))
+        
         //4
         self.scrollView.addSubview(imgThree)
         self.scrollView.addSubview(imgFour)
@@ -42,9 +50,9 @@ class MoreViewController: UIViewController {
         //self.pageControl.currentPage = 0
         
         
-        NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "moveToNextPage", userInfo: nil, repeats: true)
+        NSTimer.scheduledTimerWithTimeInterval(4, target: self, selector: "moveToNextPage", userInfo: nil, repeats: true)
  
-        */
+        
     }
     
     func switchScreenGesture(sender: UIScreenEdgePanGestureRecognizer) {
@@ -52,6 +60,25 @@ class MoreViewController: UIViewController {
         if (flag % 2 == 1){
             tabBarController?.selectedIndex = 2
         }
+    }
+    
+    func moveToNextPage (){
+        print(CGRectGetWidth(scrollView.frame))
+        print(CGRectGetHeight(scrollView.frame))
+        
+        scrollView.setContentOffset(CGPoint(x: 600, y: 0), animated: true)
+        let pageWidth:CGFloat = CGRectGetWidth(self.scrollView.frame)
+        let maxWidth:CGFloat = pageWidth * 2
+        let contentOffset:CGFloat = self.scrollView.contentOffset.x
+        print("ScrollView height = " + String(scrollView.frame.height))
+        print("ImageView3 height = " + String(imageViewHeight))
+        
+        var slideToX = contentOffset + pageWidth
+        
+        if  contentOffset + pageWidth == maxWidth{
+            slideToX = 0
+        }
+        self.scrollView.scrollRectToVisible(CGRectMake(slideToX, 0, pageWidth, CGRectGetHeight(self.scrollView.frame)), animated: true)
     }
     
     
