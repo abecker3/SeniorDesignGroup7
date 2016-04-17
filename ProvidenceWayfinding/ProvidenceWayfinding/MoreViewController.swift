568e5c665a1509e83e81c75c83a66079b4138bf4
Shaun McBurney <smcburney@zagmail.gonzaga.edu>
2016-04-17 10:23:17 -0700

    Still working on new More screen

--- ProvidenceWayfinding/ProvidenceWayfinding/MoreViewController.swift
+++ ProvidenceWayfinding/ProvidenceWayfinding/MoreViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MoreViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
     var screenEdgeRecognizer: UIScreenEdgePanGestureRecognizer!
     var flag = 0
     
@@ -19,6 +20,31 @@ class MoreViewController: UIViewController {
         screenEdgeRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: "switchScreenGesture:")
         screenEdgeRecognizer.edges = .Left
         view.addGestureRecognizer(screenEdgeRecognizer)
+        
+        let scrollViewWidth = scrollView.frame.width
+        let scrollViewHeight = scrollView.frame.height
+        //print("scrollViewWidth = " + String(scrollViewWidth))
+        //print("scrollViewHeight = " + String(scrollViewHeight))
+        
+        //3
+        let imgThree = UIImageView(frame: CGRectMake(scrollViewWidth*0, 0, scrollViewWidth, scrollViewHeight))
+        imgThree.image = UIImage(named: "Map.png")
+        let imgFour = UIImageView(frame: CGRectMake(scrollViewWidth*1, 0,scrollViewWidth, scrollViewHeight))
+        imgFour.image = UIImage(named: "Car.png")
+        print("imgFour.image.size = " + String(imgFour.image!.size))
+        /*
+        //4
+        self.scrollView.addSubview(imgThree)
+        self.scrollView.addSubview(imgFour)
+        //4
+        self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.width * 2, self.scrollView.frame.height)
+        self.scrollView.delegate = self
+        //self.pageControl.currentPage = 0
+        
+        
+        NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "moveToNextPage", userInfo: nil, repeats: true)
+ 
+        */
     }
     
     func switchScreenGesture(sender: UIScreenEdgePanGestureRecognizer) {
