fab918ab3665c823c1ce77fa8664d75f47b895aa
Derek Becker <derekbecker@derek-beckers-macbook-pro.local>
2016-04-21 10:31:36 -0700

    Fixed Bug

--- ProvidenceWayfinding/ProvidenceWayfinding/DirectoryInformation.swift
+++ ProvidenceWayfinding/ProvidenceWayfinding/DirectoryInformation.swift
@@ -108,15 +108,14 @@ func loadDirectory()
         {
             directory.append(Directory(name: String(name), category: String(category), floor: String(floor), hours: String(hours), ext: Int(ext)!, notes: String(notes)))
         }
-        
-        directory.append(Directory(name: "Doctors Building Information", category: "Doctors Building", floor: "Main", hours: "9AM to 5PM", ext: 44775, notes: ""))
-        directory.append(Directory(name: "Pediatric Gastroenterology", category: "Doctors Building", floor: "Main", hours: "8AM to 5PM Mon-Fri", ext: 45437, notes: ""))
-        directory.append(Directory(name: "Pediatric Nephrology", category: "Doctors Building", floor: "Main", hours: "8AM to 5PM Mon-Fri", ext: 3400930, notes: ""))
-        directory.append(Directory(name: "Pediatric Pulmonology", category: "Doctors Building", floor: "4", hours: "8AM to 5PM Mon-Fri", ext: 46960, notes: ""))
-        directory.append(Directory(name: "Transplant", category: "Doctors Building", floor: "Main", hours: "8AM to 5PM Mon-Fri", ext: 0, notes: ""))
-        directory.append(Directory(name: "Providence Center for Faith and Healing", category: "Faith and Healing", floor: "Main", hours: "", ext: 43008, notes: ""))
                           
     }
+    directory.append(Directory(name: "Doctors Building Information", category: "Doctors Building", floor: "Main", hours: "9AM to 5PM", ext: 44775, notes: ""))
+    directory.append(Directory(name: "Pediatric Gastroenterology", category: "Doctors Building", floor: "Main", hours: "8AM to 5PM Mon-Fri", ext: 45437, notes: ""))
+    directory.append(Directory(name: "Pediatric Nephrology", category: "Doctors Building", floor: "Main", hours: "8AM to 5PM Mon-Fri", ext: 3400930, notes: ""))
+    directory.append(Directory(name: "Pediatric Pulmonology", category: "Doctors Building", floor: "4", hours: "8AM to 5PM Mon-Fri", ext: 46960, notes: ""))
+    directory.append(Directory(name: "Transplant", category: "Doctors Building", floor: "Main", hours: "8AM to 5PM Mon-Fri", ext: 0, notes: ""))
+    directory.append(Directory(name: "Providence Center for Faith and Healing", category: "Faith and Healing", floor: "Main", hours: "", ext: 43008, notes: ""))
     
 }
 
